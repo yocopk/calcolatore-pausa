f1f057d17b48e793304fad13b5f0fbc095257f8c
yocopk <133963909+yocopk@users.noreply.github.com>
2023-11-09 23:26:50 +0100

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+function calculateTime() {
+  const inputTime = document.getElementById("input-time").value;
+  const [hours, minutes] = inputTime.split(":");
+
+  if (
+    hours &&
+    minutes &&
+    hours >= 0 &&
+    hours <= 23 &&
+    minutes >= 0 &&
+    minutes <= 59
+  ) {
+    const currentTime = new Date();
+    const futureTime = new Date(currentTime);
+    futureTime.setHours(parseInt(hours));
+    futureTime.setMinutes(parseInt(minutes) + 15);
+
+    const options = { hour: "numeric", minute: "numeric" };
+
+    document.getElementById("display-input-time").textContent = inputTime;
+    document.getElementById("display-future-time").textContent =
+      futureTime.toLocaleTimeString("it-IT", options);
+  } else {
+    alert("Inserisci un'ora valida nel formato HH:MM.");
+  }
+}
+
+const inputField = document.getElementById("input-time");
+inputField.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    calculateTime();
+  }
+});
